ab574d700e955ded2cb8d6783df480e6ee251cab
ivanBonis <ivan.bonifacio.aca@gmail.com>
2026-03-23 12:43:45 +0000

    2026-03-23 --> 12:43:45

--- Ivan/Aula5/script.js
+++ Ivan/Aula5/script.js
@@ -3,9 +3,26 @@ console.log(nome);
 
 // Aceder button btnClicar
 const btnClicar = document.querySelector("#btnClicar");
-console.log(btnClicar);
+
+const caixaTexto = document.querySelector("#caixaTexto");
+
+const titulo1 = document.querySelector("#titulo1");
+
+const lista = document.querySelector("#lista");
 
 // Criar um evento para o botao
 btnClicar.addEventListener('click', function () {
-    alert("Butao pressionado");
+    // alert("Butao pressionado");
+    titulo1.textContent = "Olá, mundo!";
+    titulo1.style.backgroundColor = "red";
+});
+
+caixaTexto.addEventListener('keypress', function (evt) {
+    if (evt.key === "Enter") {
+        console.log(caixaTexto.value);
+    }
+});
+
+lista.addEventListener('click', function (evt) {
+    evt.target.remove();
 });
